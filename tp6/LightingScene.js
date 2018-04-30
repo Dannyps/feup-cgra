e9186216ee5835fc49bc436688e48a6a8331b8d3
Daniel Silva <danny@dannyps.net>
2018-04-30 17:23:53 +0100

    Fix nmaster

--- tp6/LightingScene.js
+++ tp6/LightingScene.js
@@ -54,7 +54,7 @@ class LightingScene extends CGFscene
 		this.wood.setDiffuse(139/255,70/255,8/255,1);
 		this.wood.setSpecular(0,0,0,0);
 		this.wood.setShininess(120);
-		
+ 		
 		this.floorAppearance = new CGFappearance(this);
 		this.floorAppearance.loadTexture("../resources/images/floor.png");
 		this.floorAppearance.setAmbient(0.2,0.2,0.2,0.0);
