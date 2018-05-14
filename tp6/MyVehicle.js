51532f32da2e3cfa603f9e8302ee634887436c82
Unknown <migud.carvalho@gmail.com>
2018-05-14 18:02:17 +0100

    Textura nas rodas e estao a rodar

--- tp6/MyVehicle.js
+++ tp6/MyVehicle.js
@@ -11,6 +11,7 @@ class MyVehicle extends CGFobject {
         this.x = 0;
         this.z = 0;
         this.dir = 0;
+        this.rdir = 0;
         this.mc = 0.3; // magic const
 
         this.cubo = new MyUnitCubeQuad(this.scene);
@@ -24,6 +25,12 @@ class MyVehicle extends CGFobject {
         this.rodatd = new MyCylinder(this.scene, 30, 10);
         this.facetd = new Circle(this.scene, 30);
 
+        this.clockText = new CGFappearance(this.scene);
+    this.clockText.loadTexture("../resources/images/clock.png");
+    this.clockText.setAmbient(0.2,0.2,0.2,0.0);
+    this.clockText.setDiffuse(0.2, 0.2, 0.2, 1);
+    this.clockText.setSpecular(0.5, 0.5, 0.5,1);
+    this.clockText.setShininess(1);
 
     };
 
@@ -38,7 +45,21 @@ class MyVehicle extends CGFobject {
     move(speed) {
         this.x += this.mc * speed * Math.cos(this.dir);
         this.z -= this.mc * speed * Math.sin(this.dir);
+        if(speed>0)
+        this.rotateFront(speed);
+        else
+        this.rotateBack(speed);
     };
+
+
+
+    rotateFront(speed){
+        this.rdir -= this.mc * speed * 0.5;
+    }
+
+    rotateBack(speed){
+        this.rdir-= this.mc * speed * 0.5;
+    }
 
     display() {
         /*console.log(this.scene.getUpdatePeriod());
@@ -60,50 +81,60 @@ class MyVehicle extends CGFobject {
             this.scene.popMatrix();
 
             //rodas
-            this.scene.pushMatrix(); {
-                this.scene.translate(0, 1, 0);
 
                 //rodafe
                 this.scene.pushMatrix(); {
+                    this.scene.translate(0, 1, 0);
                     this.scene.translate(2.5, 0, -1.25);
+                        this.scene.rotate(this.rdir, 0, 0, 1);
                     this.rodafe.display();
                     this.scene.translate(0, 0, -0.5);
                     this.scene.rotate(Math.PI, 1, 0, 0);
+                    this.clockText.apply();
                     this.facefe.display();
+                    this.scene.materialDefault.apply();
 
                 }
                 this.scene.popMatrix();
 
 
                 this.scene.pushMatrix(); {
+                    this.scene.translate(0, 1, 0);
                     this.scene.translate(2.5, 0, 1.25);
+                        this.scene.rotate(this.rdir, 0, 0, 1);
                     this.rodafd.display();
                     this.scene.translate(0, 0, 0.5);
+                    this.clockText.apply();
                     this.facefd.display();
+                    this.scene.materialDefault.apply();
 
                 }
                 this.scene.popMatrix();
 
 
                 this.scene.pushMatrix(); {
+                    this.scene.translate(0, 1, 0);
                     this.scene.translate(-2.5, 0, -1.25);
+                        this.scene.rotate(this.rdir, 0, 0, 1);
                     this.rodate.display();
                     this.scene.translate(0, 0, -0.5);
                     this.scene.rotate(Math.PI, 1, 0, 0);
+                    this.clockText.apply();
                     this.facete.display();
+                    this.scene.materialDefault.apply();
                 }
                 this.scene.popMatrix();
 
                 this.scene.pushMatrix(); {
+                    this.scene.translate(0, 1, 0);
                     this.scene.translate(-2.5, 0, 1.25);
+                        this.scene.rotate(this.rdir, 0, 0, 1);
                     this.rodatd.display();
                     this.scene.translate(0, 0, 0.5);
+                    this.clockText.apply();
                     this.facetd.display();
+                    this.scene.materialDefault.apply();
                 }
-
-            }
-
-            this.scene.popMatrix();
 
             this.scene.popMatrix();
         }
