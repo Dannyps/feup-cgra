e79ab13b973582bebf50b5f193e3758a71a75320
Daniel Silva <danny@dannyps.net>
2018-05-27 15:37:44 +0100

    Add MySphere

---
+++ tpTP/MySphere.js
@@ -0,0 +1,158 @@
+/**
+ * MyTable
+ * @param gl {WebGLRenderingContext}
+ * @constructor
+ */
+
+class MySphere extends CGFobject
+{
+	constructor(scene, slices, stacks, sr = 1, tr = 1)
+	{
+        super(scene);
+        this.slices = slices;
+        this.stacks = stacks;
+        this.sr=sr;
+        this.tr=tr;
+        
+        this.initBuffers();
+
+    };
+    
+    addToIndices(v){
+        if ( typeof this.addToIndices.counter == 'undefined' ) {
+            // It has not... perform the initialization
+            this.addToIndices.counter = 0;
+            this.addToIndices.counterT = 0;
+        }
+        //console.log("Adding "+Math.floor(this.addToIndices.counterT++/3) +"th index "+v + " to sphere.");
+        this.indices.push(v);
+
+        if(++this.addToIndices.counter==3){
+            //console.log("\n");
+            this.addToIndices.counter=0;
+        }
+    };
+    addToNormals(v){
+        if ( typeof this.addToNormals.counter == 'undefined' ) {
+            // It has not... perform the initialization
+            this.addToNormals.counter = 0;
+        }
+        //console.log("Adding normal "+v + " to sphere.");
+        this.normals.push(v);
+        if(++this.addToNormals.counter==3){
+            //console.log("\n");
+            this.addToNormals.counter=0;
+        }
+    };
+    addToVertices(v){
+        if ( typeof this.addToVertices.counter == 'undefined' ) {
+            // It has not... perform the initialization
+            this.addToVertices.counter = 0;
+            this.addToVertices.counterT = 0;
+        }
+        //console.log("Adding "+Math.floor(this.addToVertices.counterT++/3) +"th vertix "+v + " to sphere.");
+        this.vertices.push(v);
+        if(++this.addToVertices.counter==3){
+            //console.log("\n");
+            this.addToVertices.counter=0;
+        }
+    };
+
+    initBuffers() 
+	{
+        // Define vertices
+        this.vertices = [];
+        
+        let angleIncrement=2*Math.PI/this.slices;
+        let angle2Increment=Math.PI/this.stacks;
+        let zInc=1/this.stacks*2;
+        let z = 1;
+        let angle2=0;
+        for(let i = 0; i <= this.stacks; i++){
+            //console.log(z);
+            let angle = 0;
+            
+
+            for(let j = 0; j < this.slices; j++){
+                this.addToVertices(Math.cos(angle)*Math.sin(angle2));
+                this.addToVertices(Math.sin(angle)*Math.sin(angle2));
+                this.addToVertices(z*1.6);
+                angle+=angleIncrement;
+            }
+            angle2+=angle2Increment;
+            z-=zInc*Math.sin(angle2);
+            
+        }
+
+        this.normals = [];
+        let angle=0;
+        angle2=0;
+
+        for(let i = 0; i <= this.stacks; i++){;
+            let angle = 0;
+            
+            for(let j = 0; j < this.slices; j++){
+                this.addToNormals(Math.cos(angle)*Math.sin(angle2));
+                this.addToNormals(Math.sin(angle)*Math.sin(angle2));
+                this.addToNormals(Math.cos(angle2));
+                angle+=angleIncrement;
+            }
+            angle2+=angle2Increment;
+            z-=zInc*Math.sin(angle2);
+            
+        }
+
+        // Define indices
+        this.indices = [];
+
+        for(let i = 0; i < this.stacks;i++){
+            
+
+            for(var j = i*this.slices; j < this.slices*(i+1); j++){
+                if(j+1==this.slices*(i+1)){
+                    this.addToIndices(i*this.slices);
+                }else{
+                    this.addToIndices(j+1);
+                }
+                this.addToIndices(j);
+                this.addToIndices(j+this.slices);
+            }
+            //debugger;
+
+            for(var j = i*this.slices; j < this.slices*(i+1); j++){
+                                
+                this.addToIndices(j+this.slices);
+                
+                if(j+this.slices+1==this.slices*(i+2)){
+                    this.addToIndices((i+1)*this.slices);
+                }else{
+                    this.addToIndices(j+this.slices+1);
+                }
+
+                if(j+1==this.slices*(i+1)){
+                    this.addToIndices(i*this.slices);
+                }else{
+                    this.addToIndices(j+1);
+                }
+
+                
+            }
+            //debugger;
+        }
+
+        this.texCoords = [];
+
+        for(let i = 0; i<=this.stacks;i++){
+            let t=(this.stacks-i)/this.stacks;
+            for(let j = 0; j < this.slices;j++){
+                let s=(this.slices-1-j)/(this.slices-1);
+                this.texCoords.push(s*this.sr,t*this.tr);
+            }
+        }
+
+        //console.log(this.texCoords);
+    
+        this.primitiveType = this.scene.gl.TRIANGLES;
+		this.initGLBuffers();
+	};
+};
